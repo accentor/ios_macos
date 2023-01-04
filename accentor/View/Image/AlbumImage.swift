3043b6b72fc32c36cc28f3feb901c709fcfb484e
Robbe Van Petegem <robbe@vanpetegem.me>
2023-01-04 15:57:26 +0100

    Add `CachedAsyncImage` package

---
+++ accentor/View/Image/AlbumImage.swift
@@ -0,0 +1,20 @@
+//
+//  AlbumImage.swift
+//  accentor
+//
+//  Created by Robbe Van Petegem on 04/01/2023.
+//
+
+import SwiftUI
+
+struct AlbumImage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AlbumImage_Previews: PreviewProvider {
+    static var previews: some View {
+        AlbumImage()
+    }
+}
---
+++ accentor/View/Image/ArtistImage.swift
@@ -0,0 +1,20 @@
+//
+//  ArtistImage.swift
+//  accentor
+//
+//  Created by Robbe Van Petegem on 04/01/2023.
+//
+
+import SwiftUI
+
+struct ArtistImage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ArtistImage_Previews: PreviewProvider {
+    static var previews: some View {
+        ArtistImage()
+    }
+}
