4336a4ae13cd8d9213ce6456df73415a51bed0a0
Robbe Van Petegem <robbe@vanpetegem.me>
2023-03-11 11:04:03 +0100

    Calculate `albumArtistsText` and `trackArtistsText` inside models (#23)
    * Move `trackArtistsName` to model
    
    * Calculate albumArtistText in model

--- accentor/View/Albums/AlbumCard.swift
+++ accentor/View/Albums/AlbumCard.swift
@@ -10,25 +10,12 @@ import CachedAsyncImage
 
 struct AlbumCard: View {
     var album: Album
-    
-    func albumArtistsText() -> String {
-        guard let albumArtists = album.albumArtists else { return "Various Artists" }
-        
-        let sorted = albumArtists.sortedArray(using: [NSSortDescriptor(key: "order", ascending: true)])
-        return sorted.reduce("", { acc, cur in
-            // NOTE: I'm note sure why this reducer looses the context if which type `cur`
-            // For now we simply force this to be an album artist
-            let aa = cur as! AlbumArtist
-            return "\(acc)\(aa.name ?? "")\(aa.separator ?? "")"
-        })
-    }
 
-    
     var body: some View {
         VStack(alignment: .leading) {
             AlbumImage(album: album)
             Text(album.title ?? "")
-            Text(albumArtistsText())
+            Text(album.albumArtistsText)
             Spacer()
         }.onTapGesture {
             PlayQueue.shared.addAlbumToQueue(album: album, replace: true)
