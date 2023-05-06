c0d0067d19e15fa4987ccde8577f5214936d602d
Robbe Van Petegem <robbe@vanpetegem.me>
2023-05-06 10:42:37 +0200

    Add now playing notification (#30)
    * Store player state in enum
    
    * Simplify method
    
    * Allow remote commands for player
    
    * Report `NowPlayingInfo` when playerItem or playbackStatus changes

--- accentor/View/Player/PlayerViewModel.swift
+++ accentor/View/Player/PlayerViewModel.swift
@@ -9,11 +9,30 @@ import SwiftUI
 import AVFAudio
 import AVFoundation
 import Combine
+import MediaPlayer
 
 class PlayerViewModel: NSObject, ObservableObject {
-    @Published var playing: Bool = false
+    // Possible values of `playerState`
+    // The raw values match those of `MPNowPlayingPlaybackState`
+    enum PlayerState: UInt {
+        case stopped = 3
+        case playing = 1
+        case paused = 2
+    }
+    
+    /// This property is published, so that changes in this state triggers a reload in the view
+    /// The view should use the computed `playing` property,
+    @Published private var playerState: PlayerState = .stopped {
+        didSet { self.handlePlaybackChange() }
+    }
+    
+
     @Published var playingTrack: Track?
     @Published var playQueue: PlayQueue = PlayQueue.shared
+
+    var playing: Bool {
+        get { return self.playerState == .playing }
+    }
     var canPlay: Bool {
         get { return playQueue.queue.count > 0 }
     }
@@ -29,10 +48,15 @@ class PlayerViewModel: NSObject, ObservableObject {
     public static let shared = PlayerViewModel()
 
     private var player: AVPlayer = AVPlayer()
+    private var statusObserver: NSObjectProtocol!
     private var cancellables: Set<AnyCancellable> = []
     
     override init() {
         super.init()
+        // Mark player to allow external playback
+        player.allowsExternalPlayback = true
+        setupCommandCenter()
+
         playQueue.$currentIndex.sink { [weak self] newIndex in
             guard let self = self else { return }
             
@@ -41,6 +65,10 @@ class PlayerViewModel: NSObject, ObservableObject {
                 self.setPlayingTrack(queueItem: self.playQueue.currentTrack)
             }
         }.store(in: &cancellables)
+        
+        statusObserver = player.observe(\.currentItem?.status, options: .initial) {
+            [unowned self] _, _ in self.handlePlaybackChange()
+        }
         
         NotificationCenter.default
             .addObserver(self,
@@ -61,22 +89,22 @@ class PlayerViewModel: NSObject, ObservableObject {
             return
         }
 
-        if self.playing {
-            self.pause()
-        } else {
-            self.play()
+        switch self.playerState  {
+        case .playing: self.pause()
+        case .paused: self.play()
+        case .stopped: self.play()
         }
     }
     
     func pause() {
         self.player.pause()
-        self.playing = false
+        self.playerState = .paused
     }
     
     func stop() {
         self.player.pause()
         self.player.seek(to: CMTime(value: 0, timescale: 1))
-        self.playing = false
+        self.playerState = .stopped
     }
     
     func next() {
@@ -95,16 +123,16 @@ class PlayerViewModel: NSObject, ObservableObject {
             #if os(iOS)
             // Configure and activate the AVAudioSession
             // This is only available on iOS
-            try AVAudioSession.sharedInstance().setCategory(
-                AVAudioSession.Category.playback
-            )
+            try AVAudioSession.sharedInstance().setCategory(.playback)
 
             try AVAudioSession.sharedInstance().setActive(true)
             #endif
             
             
             self.player.play()
-            DispatchQueue.main.async { self.playing = true }
+            DispatchQueue.main.async {
+                self.playerState = .playing
+            }
         }
         catch {
             print("Error while playing")
@@ -124,8 +152,78 @@ class PlayerViewModel: NSObject, ObservableObject {
 
         guard self.playingTrack != nil else { self.stop(); return }
 
+        self.handlePlayerItemChange()
         self.player.replaceCurrentItem(with: queueItem?.playerItem())
         player.automaticallyWaitsToMinimizeStalling = false
         self.play()
+    }
+
+    // MARK: Now playing info
+    private func handlePlayerItemChange() {
+        guard playerState != .stopped else { return }
+        // Find current item
+        guard let currentTrack = self.playingTrack else { return }
+
+        let nowPlayingInfoCenter = MPNowPlayingInfoCenter.default()
+        var nowPlayingInfo = [String: Any]()
+        
+        // Set track info
+        nowPlayingInfo[MPMediaItemPropertyTitle] = currentTrack.title
+        nowPlayingInfo[MPMediaItemPropertyArtist] = currentTrack.trackArtistsText
+        nowPlayingInfo[MPMediaItemPropertyAlbumArtist] = currentTrack.album?.albumArtistsText
+        nowPlayingInfo[MPMediaItemPropertyAlbumTitle] = currentTrack.album?.title
+        nowPlayingInfo[MPMediaItemPropertyPlaybackDuration] = currentTrack.length
+        
+        // Set playing metadata
+        nowPlayingInfo[MPNowPlayingInfoPropertyMediaType] = MPMediaType.music.rawValue
+
+        nowPlayingInfoCenter.nowPlayingInfo = nowPlayingInfo
+    }
+    
+    private func handlePlaybackChange() {
+        MPNowPlayingInfoCenter.default().playbackState = MPNowPlayingPlaybackState(rawValue: playerState.rawValue)!
+
+        guard let currentItem = player.currentItem else { return }
+        guard currentItem.status == .readyToPlay else { return }
+
+        let nowPlayingInfoCenter = MPNowPlayingInfoCenter.default()
+        var nowPlayingInfo = nowPlayingInfoCenter.nowPlayingInfo ?? [String: Any]()
+        
+        nowPlayingInfo[MPNowPlayingInfoPropertyElapsedPlaybackTime] = Float(currentItem.currentTime().seconds)
+        nowPlayingInfoCenter.nowPlayingInfo = nowPlayingInfo
+    }
+    
+    // MARK: Remote commands
+    enum RemoteCommand {
+        case play
+        case pause
+        case stop
+        case togglePausePlay
+        case nextTrack
+        case previousTrack
+    }
+
+    private func setupCommandCenter() {
+        let commandCenter = MPRemoteCommandCenter.shared()
+        commandCenter.playCommand.addTarget { self.handleRemoteCommand(command: RemoteCommand.play, event: $0) }
+        commandCenter.pauseCommand.addTarget { self.handleRemoteCommand(command: RemoteCommand.pause, event: $0) }
+        commandCenter.stopCommand.addTarget { self.handleRemoteCommand(command: RemoteCommand.stop, event: $0) }
+        commandCenter.togglePlayPauseCommand.addTarget { self.handleRemoteCommand(command: RemoteCommand.togglePausePlay, event: $0) }
+        commandCenter.nextTrackCommand.addTarget { self.handleRemoteCommand(command: RemoteCommand.nextTrack, event: $0) }
+        commandCenter.previousTrackCommand.addTarget { self.handleRemoteCommand(command: RemoteCommand.previousTrack, event: $0) }
+        
+    }
+    
+    private func handleRemoteCommand(command: RemoteCommand, event: MPRemoteCommandEvent) -> MPRemoteCommandHandlerStatus {
+        switch command {
+        case .pause: pause()
+        case .play: play()
+        case .stop: stop()
+        case .togglePausePlay: togglePlaying()
+        case .nextTrack: next()
+        case .previousTrack: prev()
+        }
+        
+        return .success
     }
 }
