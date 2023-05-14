958c6391e8ebf47574280b01607e44fb743e9a1b
Robbe Van Petegem <robbe@vanpetegem.me>
2023-05-14 13:57:12 +0200

    Allow users to refresh data through commands (#43)

--- accentor/accentorApp.swift
+++ accentor/accentorApp.swift
@@ -16,5 +16,17 @@ struct accentorApp: App {
             ContentView()
                 .environment(\.managedObjectContext, persistenceController.container.viewContext)
         }
+        .commands {
+            CommandMenu("Data") {
+                Button(action: {
+                    AlbumService.shared.index(context: persistenceController.container.viewContext)
+                    ArtistService.shared.index(context: persistenceController.container.viewContext)
+                    TrackService.shared.index(context: persistenceController.container.viewContext)
+                    UserDefaults.standard.set(Date().timeIntervalSince1970, forKey: "last_sync_finished")
+                }, label: {
+                    Text("Refresh")
+                })
+            }
+        }
     }
 }
