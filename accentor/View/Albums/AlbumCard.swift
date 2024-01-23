bd0c8be7e5b161561fe1e01f7546b34bb66fce6d
Robbe Van Petegem <git@robbevp.be>
2024-01-23 20:34:25 +0100

    Improve layout of album, so that we always show a square with fill size artwork

--- accentor/View/Albums/AlbumCard.swift
+++ accentor/View/Albums/AlbumCard.swift
@@ -16,22 +16,32 @@ struct AlbumCard: View {
             AlbumCardViewModel(database: $0.appDatabase, id: id)
         }
     }
-
     var body: some View {
         if let album = viewModel.albumInfo?.album {
             VStack(alignment: .leading) {
-                CachedImage(imageURL: album.image250) {
-                    ZStack {
-                        Rectangle().fill(.gray)
-                        Image(systemName: "music.note").font(.largeTitle)
-                    }
-                }.aspectRatio(1, contentMode: .fit)
-                Text(album.title)
-                Text(AlbumArtist.constructAlbumArtistText(viewModel.albumInfo?.albumArtists))
+                GeometryReader { geometry in
+                    CachedImage(imageURL: album.image250) {
+                        ZStack {
+                            Rectangle().fill(.gray)
+                            Image(systemName: "music.note").font(.largeTitle)
+                        }
+                    }.scaledToFill()
+                    .clipped()
+                    .frame(width: geometry.size.width, height: geometry.size.width)
+                    
+                    .clipShape(RoundedRectangle(cornerRadius: 4))
+                    .shadow(radius: 6, x: -3, y: 3)
+                    
+                }.frame(maxWidth: .infinity).aspectRatio(1, contentMode: .fit)
+                VStack(alignment: .leading) {
+                    Text(album.title).lineLimit(2)
+                    Text(AlbumArtist.constructAlbumArtistText(viewModel.albumInfo?.albumArtists)).foregroundColor(.gray).lineLimit(1)
+                }.truncationMode(.tail).font(.system(size: 12))
+                
                 Spacer()
             }.onTapGesture {
                 Task { await viewModel.queueAlbum() }
-            }
+            }.padding(.horizontal, 6)
         } else {
             ProgressView()
         }
