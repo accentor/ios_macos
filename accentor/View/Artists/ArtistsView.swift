a5ae085a3a72b6d099279afaac9d9ba86d06089e
Robbe Van Petegem <git@robbevp.be>
2024-02-05 21:06:09 +0100

    Use `scrollTargetLayout` and `scrollTargetBehaviour` where possible (#65)
    * Use `scrollTargetLayout` and `scrollTargetBehaviour` where possible
    
    * Bump horizontal padding for section title

--- accentor/View/Artists/ArtistsView.swift
+++ accentor/View/Artists/ArtistsView.swift
@@ -11,7 +11,7 @@ import GRDBQuery
 
 struct ArtistsView: View {
     @EnvironmentStateObject private var viewModel: ArtistsViewModel
-
+    
     init() {
         _viewModel = EnvironmentStateObject {
             ArtistsViewModel(database: $0.appDatabase)
@@ -21,7 +21,7 @@ struct ArtistsView: View {
     let columns = [
         GridItem(.adaptive(minimum: 162, maximum: 252))
     ]
-
+    
     var body: some View {
         ScrollView {
             LazyVGrid(columns: columns) {
@@ -30,8 +30,22 @@ struct ArtistsView: View {
                         ArtistCard(artist: artist)
                     }.buttonStyle(PlainButtonStyle())
                 }
-            }.padding(EdgeInsets(top: 20, leading: 30, bottom: 65, trailing: 30))
+            }.modify {
+                if #available(macOS 14.0, iOS 17.0, *) {
+                    $0.scrollTargetLayout()
+                } else {
+                    $0.padding(EdgeInsets(top: 4, leading: 30, bottom: 65, trailing: 30))
+                }
+            }
+            
+        }.modify {
+            if #available(macOS 14.0, iOS 17.0, *) {
+                $0.scrollTargetBehavior(.viewAligned)
+                    .safeAreaPadding(EdgeInsets(top: 4, leading: 30, bottom: 65, trailing: 30))
+            } else {
+                $0
+            }
         }.navigationTitle("Artists")
-        .searchable(text: $viewModel.searchTerm)
+            .searchable(text: $viewModel.searchTerm)
     }
 }
