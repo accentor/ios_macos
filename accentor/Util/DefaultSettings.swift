e8af05b8dc267a0631261b5b21665165c820cde1
Robbe Van Petegem <git@robbevp.be>
2025-08-03 16:39:39 +0200

    Make the default codec conversion variable with compiler flags (#79)

--- accentor/Util/DefaultSettings.swift
+++ accentor/Util/DefaultSettings.swift
@@ -8,5 +8,9 @@
 import Foundation
 
 enum DefaultSettings {
+    #if DEBUG
+    static let codecConversionId = 1
+    #else
     static let codecConversionId = 3 // The MP3 320 codec in our current production ENV
+    #endif
 }
